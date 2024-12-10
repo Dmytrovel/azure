a5dba7b100aacbdf74674b2a0d74c1ba09711a26
DIma <dmytrovelychko@gmail.com>
2024-12-10 11:56:04 +0000

    fix

--- versions.tf
+++ versions.tf
@@ -11,6 +11,9 @@ terraform {
 
 provider "azurerm" {
   subscription_id = "7f408f98-f52a-403a-84e3-aa3401b28e30"
+  use_oidc = true
+  client_id = "c41f3068-05e1-43ac-909b-95b7190aa073"
+  tenant_id = "e66b0a1e-12b0-4051-8027-930b18b8d022"
   features {}
 }
 
